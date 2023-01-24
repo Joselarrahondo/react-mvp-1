a475e540ed7125a6fe5d83f0e299340dac13159b
Joselarrahondo <jlarraho@gmail.com>
2023-01-23 18:11:33 -0600

    updated search feature

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import Restaurants from "./Restaurants.jsx";
-import {useRecoilState} from "recoil"
+import { useRecoilState } from "recoil";
 
 function App() {
   const [restaurants, setRestaurants] = useState([]);
-  const [searchResults, setSearchResults] = useState("")
-  const [reRender, setReRender] = useState(false)
-  /// change use effect to fucntion that called when user clicks search button ///
+  const [searchResults, setSearchResults] = useState("");
+  const [reRender, setReRender] = useState(false);
+
   useEffect(() => {
-    let foodType = searchResults
+    let foodType = searchResults;
     fetch(`http://localhost:8000/restaurant/${foodType}`)
       .then((res) => {
         return res.json();
       })
       .then((data) => {
         // console.log(data);
-        setRestaurants(data)
+        setRestaurants(data);
       });
   }, []);
-  return <div className="App" > 
-  <Restaurants setReRender={setReRender} restaurants={restaurants} setRestaurants={setRestaurants} searchResults={searchResults} />
-  </div>;
+  return (
+    <div className="App">
+      <Restaurants
+        setReRender={setReRender}
+        restaurants={restaurants}
+        setRestaurants={setRestaurants}
+        searchResults={searchResults}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/Restaurants.jsx
+++ src/Restaurants.jsx
@@ -1,85 +1,77 @@
 
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
-import { render } from '@testing-library/react';
 
 const Restaurants = (props) => {
-    //////// Creating State to use for show all button ////////
-    const [showAll, setShowAll] = useState(false)
-    const [cuisine, setCuisine] = useState("")
- 
-// useEffect( () => {
-//      console.log("reRender")   
-// }, [reRender])
-    
-const searchRestaurant = async (e) => {
-    e.preventDefault();
-    let { data } = await axios.get(`http://localhost:8000/restaurant/${cuisine}`)
-    
-    let filterdata = data.filter(restaurant =>
-        restaurant.foodtype === cuisine
-    )
-    props.setRestaurants(filterdata)
+    const [cuisine, setCuisine] = useState("");
+    const [restaurantData, setRestaurantData] = useState([]);
+useEffect(() => {
+    searchRestaurant();
+},[])
+    const searchRestaurant = async () => {
+        let { data } = await axios.get(`http://localhost:8000/restaurant/${cuisine}`)
+        setRestaurantData(data)
 
-    if (cuisine === "") {
-        alert("please provide foodtype")
-    }
-    else if(filterdata.length === 0){
-        alert("foodtype not available")
+
     };
-        setShowAll(!showAll)
-        props.setReRender(!props.reRender)
-};
+    ///////// updating sate of cuisine to user input ////////
 
     const handleCuisineChange = (e) => {
-        // e.preventDefault();
-        console.log(cuisine)
+        e.preventDefault();
         setCuisine(e.target.value);
 
     }
-    const handleCuisineBtn = () => {
-        searchRestaurant(cuisine)
+
+    ////////// invokes searchRestaurant once submit button is clicked && changes showAll state to false /////
+
+    const handleCuisineBtn = (e) => {
+        e.preventDefault()
+        
+        searchRestaurant()
+
     }
+
+    ////////// Shows all restaurants in database ///////
 
     const showAllBtn = () => {
-        setShowAll(!showAll);
+       
+        searchRestaurant()
+
     }
 
+
     return (
-        <div className="results">
-            <form onSubmit={searchRestaurant}>
+        <div className="results" >
+            <div>
+            <form onSubmit={handleCuisineBtn} >
                 <input onChange={handleCuisineChange} value={cuisine} type="text" placeholder="search restaurants" />
-                <input className='seachBtn' type="submit" value="submit" />
+                 <button className='searchBtn' type="submit">SUBMIT</button>
             </form>
+            </div>
+         
 
-            <button onClick={showAllBtn} >Show all</button>
-            {showAll ? props.restaurants && props.restaurants.map((restaurant) => {
+        
+
+
+            {/** If showAll true map restaurants and display them */}
+            
+      
+
+            {/** if restaurant data is not empty display queried restaurants  */}
+        <div className='renderResults'>
+            {restaurantData ? restaurantData.map((restaurantData) => {
                 return (
-
                     <div
-                        key={restaurant.id}>
-                        <h2>{restaurant.name}</h2>
-                        <p>{restaurant.foodtype}</p>
-                        <p>{restaurant.address}</p>
+                    className="singleRestaurant"
+                        key={restaurantData.id}>
+                        <h2>{restaurantData.name}</h2>
+                        <p>{restaurantData.foodtype}</p>
+                        <p>{restaurantData.address}</p>
                     </div>
-
                 )
 
             }) : null}
-
-            {/* {props.restaurants && props.restaurants.map((restaurant) => {
-                return (
-
-                    <div
-                        key={restaurant.id}>
-                        <h2>{restaurant.name}</h2>
-                        <p>{restaurant.foodtype}</p>
-                        <p>{restaurant.address}</p>
-                    </div>
-
-                )
-
-            })} */}
+        </div>
         </div>
     )
 };
